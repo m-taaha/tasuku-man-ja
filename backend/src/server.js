4dcc4a23c69b22536c54ce98bcc5bd8f0417d38d
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-10-18 14:31:27 +0530

    add mongoDB connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,21 @@
-import express from "express"
+import dotenv from 'dotenv';
+import express from "express";
+import cookieParser from "cookie-parser"
+import connectDB from "./config/db.js"
+
+
+dotenv.config();
 
 
 const app = express();
 const PORT = 8000;
 
+app.use(express.json()); // to parse json
+app.use(express.urlencoded({exptended: true}));
+app.use(cookieParser());
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on: https://localhost:${PORT}`);
+    connectDB();
 })
