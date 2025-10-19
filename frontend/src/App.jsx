e35f0402e0310e76d3129326cf0d1f97145d2738
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-10-20 01:52:15 +0530

    setup: initialized Vite + Tailwind + shadcn UI for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {Button} from "./components/ui/button"
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
