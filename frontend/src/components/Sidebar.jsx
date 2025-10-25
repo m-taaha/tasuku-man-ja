f142fecd80566b6779f1874151204a2e560a4db1
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-10-26 00:49:00 +0530

    (frontend): add logout feature

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import { Button } from './ui/button'
 import {ClipboardList, LogOut, PlusCircle} from 'lucide-react'
+import { toast } from "sonner";
+import { logoutUser } from "../utils/api/api";
+import { useNavigate } from "react-router-dom";
 
 export default function Sidebar() {
+
+  const navigate = useNavigate();
+  
+    const handleLogout = async () => {
+      try{
+        const res = await logoutUser();
+        toast.success(res.message || "Logged out Successfully")
+        navigate("/auth");
+      }catch(error){
+        toast.error("Logout failed: Try again")
+        console.log(error);
+      }
+    }
+
   return (
     <aside className="w-64 bg-white border-r border-neutral-200 p-6 flex flex-col justify-between">
       <div>
@@ -16,16 +33,18 @@ export default function Sidebar() {
           </Button>
 
           <Button variant="ghost" className="w-full justify-start gap-2">
-            <PlusCircle className="w-4 h-4" />New Task
+            <PlusCircle className="w-4 h-4" />
+            New Task
           </Button>
         </nav>
       </div>
 
       <Button
-      variant="ghost"
-      className="w-full justify-start gap-2 text-red-500 hover:text-red-600"
+        onClick={handleLogout}
+        variant="ghost"
+        className="w-full justify-start gap-2 text-red-500 hover:text-red-600"
       >
-        <LogOut className='w-4 h-4' /> Logout
+        <LogOut className="w-4 h-4" /> Logout
       </Button>
     </aside>
   );
