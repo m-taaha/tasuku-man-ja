980b25876f17d69e47ada54eb43b5be1ce074ae3
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-10-27 23:03:28 +0530

    (backend): add getTaskById

--- backend/src/controllers/task.controller.js
+++ backend/src/controllers/task.controller.js
@@ -43,3 +43,23 @@ export const getAllTasks = async(req, res) => {
         });
     }
 }
+
+export const getTaskById = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const task = await Task.findOne({_id: id, author: req.user._id});
+
+        if(!task){
+            return res.status(404).json({
+                message: "Task not found"
+            })
+        }
+            return res.status(200).json({task})
+            
+    }catch(error){
+        console.log("Server Error:" , error);
+        return res.status(500).json({
+            message: "Server Error"
+        })
+    }
+}
--- backend/src/routes/task.routes.js
+++ backend/src/routes/task.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isAuthenticated } from '../middlewares/auth.middleware.js';
-import { createTask, getAllTasks } from '../controllers/task.controller.js';
+import { createTask, getAllTasks, getTaskById } from '../controllers/task.controller.js';
 
 
 const taskRouter = express.Router();
@@ -8,5 +8,6 @@ const taskRouter = express.Router();
 // protected route
 taskRouter.post('/create',isAuthenticated, createTask)
 taskRouter.get('/tasks',isAuthenticated, getAllTasks)
+taskRouter.get('/tasks/:id',isAuthenticated, getTaskById)
 
 export default taskRouter;
